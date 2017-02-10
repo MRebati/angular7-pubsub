70a527010cb4754bfafbceecbaa9ed0465c9f49c
sqlProvider <sqlprovider@sqlProvider.local>
2017-02-10 18:06:39 +0300

    [Extends] added interfaces

--- angular2-pubsub.service.ts
+++ angular2-pubsub.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Injectable()
-export class PubSubService {
+export class PubSubService implements IPubSubService{
 	private events = {};	
 
 	constructor() { }
 
-	$sub(event: string, callback: (value: any) => void, error?: (error: any) => void, complete?: () => void) {
+	$sub(event: string): Observable<any>;
+	$sub(event: string, callback: (value: any) => void): Subscription;	
+	$sub(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
+	$sub(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
+	$sub(event: string, callback?: (value: any) => void, error?: (error: any) => void, complete?: () => void){
 		if (event == undefined) { 
 			console.warn('[PubSub Service] => Subscription method must get event name.');
 			return;
@@ -16,10 +21,11 @@ export class PubSubService {
 
 		if (this.events[event] === undefined)
 			this.events[event] = new BehaviorSubject<any>(0);
+		
 		if (!callback || typeof callback !== 'function')
-			return <Observable<any>>this.events[event].asObservable();
+			return this.events[event].asObservable();
 		else	
-			return (<Observable<any>>this.events[event].asObservable()).subscribe(callback, error, complete);
+			return this.events[event].asObservable().subscribe(callback, error, complete);
 	}
 	$pub(event: string, eventObject?: any) {
 		if (event == undefined) {
@@ -31,6 +37,19 @@ export class PubSubService {
 			return;
 		}
 		
-		this.events[event].next(eventObject);
+		this.events[event].next(eventObject);		
 	}	
 }
+
+interface IPubSubService{
+	$pub(event: string, eventObject?: any);
+	$sub: I$sub;
+}
+
+interface I$sub{
+	(event: string): Observable<any>;
+	(event: string, callback: (value: any) => void): Subscription;
+	(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
+	(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
+}
+
