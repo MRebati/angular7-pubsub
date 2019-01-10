bd0329c5b84c3435cf8772044559e551e5203767
Mohammad Rebati <m.rebati@icloud.com>
2019-01-10 12:41:22 +0330

    documentation on exported methods

--- src/angular7-pubsub.module.ts
+++ src/angular7-pubsub.module.ts
@@ -1,6 +1,10 @@
 import { PubSubService } from './angular7-pubsub.service';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 
+/**
+ * Base Module needed to use PubSubService.
+ * @constructor
+ */
 @NgModule()
 export class PubSubModule {
   public static forRoot(): ModuleWithProviders {
--- src/angular7-pubsub.service.ts
+++ src/angular7-pubsub.service.ts
@@ -5,52 +5,135 @@ const ServiceName: string = 'PubSub Service';
 
 @Injectable()
 export class PubSubService implements IPubSubService {
-	private events = { };
+  private events = {};
 
-	constructor() { }
+  constructor() {}
 
-	public $sub(event: string): Observable<any>;
-	public $sub(event: string, callback: (value: any) => void): Subscription;
-	public $sub(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
-	public $sub(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
-	public $sub(event: string, callback?: (value: any) => void, error?: (error: any) => void, complete?: () => void) {
-		if (!event) {
-			throw new Error(`[${ServiceName}] => Subscription method must get event name.`);
-		}
+  /**
+   * a subscription which returns everything as an observable from @see {RxJs}
+   * this method does not use eventObject
+   * @param {event} event - the specific name to subscribe on - the names must be specific to those we publish.
+   * @returns {Observable<any>} an observable.
+   * @summary remember to unsubscribe in onDestroy of the components
+   */
+  public $sub(event: string): Observable<any>;
 
-		if (this.events[event] === undefined) {
-			this.events[event] = new ReplaySubject<any>();
-		}
+  /**
+   * a subscription which returns the callback, error and complete as a subscription from @see {RxJs}
+   * this method does not use eventObject
+   * @param {event} event - the specific name to subscribe on - the names must be specific to those we publish.
+   * @param {(value: any) => void} callback - callback method which gets the value from eventObject.
+   * @returns {Subscription} a subscription which we can listen to.
+   * @summary remember to unsubscribe in onDestroy of the components
+   */
+  public $sub(event: string, callback: (value: any) => void): Subscription;
 
-		if (typeof callback !== 'function') {
-			return this.events[event].asObservable();
-		} else {
-			return this.events[event].asObservable().subscribe(callback, error, complete);
-		}
-	}
+  /**
+   * a subscription which returns the callback, error and complete as a subscription from @see {RxJs}
+   * this method does not use eventObject
+   * @param {event} event - the specific name to subscribe on - the names must be specific to those we publish.
+   * @param {(error: any) => void} error - exception catch function
+   * @param {() => void} complete - complete function
+   * @returns {Subscription} a subscription which we can listen to.
+   * @summary remember to unsubscribe in onDestroy of the components
+   */
+  public $sub(
+    event: string,
+    callback: (value: any) => void,
+    error: (error: any) => void
+  ): Subscription;
 
-	public $pub(event: string, eventObject?: any) {
-		if (!event) {
-			throw new Error(`[${ServiceName}] => Publish method must get event name.`);
-		} else if (!this.events[event]) {
-			return;
-		}
+  /**
+   * a subscription which returns the callback, error and complete as a subscription from @see {RxJs}
+   * @param {event} event - the specific name to subscribe on - the names must be specific to those we publish.
+   * @param {(value: any) => void} callback - callback function
+   * @param {(error: any) => void} error - exception catch function
+   * @param {() => void} complete - complete function
+   * @returns {Subscription} a subscription which we can listen to.
+   * @summary remember to unsubscribe in onDestroy of the components
+   */
+  public $sub(
+    event: string,
+    callback: (value: any) => void,
+    error: (error: any) => void,
+    complete: () => void
+  ): Subscription;
 
-		this.events[event].next(eventObject);
-	}
+  /**
+   * a void function when we don't want to use the subscription from 'RxJs';
+   * @param {event} event - the specific name to subscribe on - the names must be specific to those we publish.
+   * @param {(value: any) => void} callback - callback function
+   * @param {(error: any) => void} error - exception catch function
+   * @param {() => void} complete - complete function
+   */
+  public $sub(event: string,callback?: (value: any) => void,error?: (error: any) => void,complete?: () => void
+  ) {
+    if (!event) {
+      throw new Error(
+        `[${ServiceName}] => Subscription method must get event name.`
+      );
+    }
+
+    if (this.events[event] === undefined) {
+      this.events[event] = new ReplaySubject<any>();
+    }
+
+    if (typeof callback !== 'function') {
+      return this.events[event].asObservable();
+    } else {
+      return this.events[event]
+        .asObservable()
+        .subscribe(callback, error, complete);
+    }
+  }
+
+  /**
+   * Base Module needed to use PubSubService.
+   * @param {event} event - the specific name to subscribe on
+   * @param {eventObject} eventObject - the optional paramter to send when raising the event
+   */
+  public $pub(event: string, eventObject?: any) {
+    if (!event) {
+      throw new Error(
+        `[${ServiceName}] => Publish method must get event name.`
+      );
+    } else if (!this.events[event]) {
+      return;
+    }
+
+    this.events[event].next(eventObject);
+  }
 }
 
 export interface IPubSubService {
-	$pub(event: string, eventObject?: any): void;
-	$sub(event: string): Observable<any>;
-	$sub(event: string, callback: (value: any) => void): Subscription;
-	$sub(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
-	$sub(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
+  $pub(event: string, eventObject?: any): void;
+  $sub(event: string): Observable<any>;
+  $sub(event: string, callback: (value: any) => void): Subscription;
+  $sub(
+    event: string,
+    callback: (value: any) => void,
+    error: (error: any) => void
+  ): Subscription;
+  $sub(
+    event: string,
+    callback: (value: any) => void,
+    error: (error: any) => void,
+    complete: () => void
+  ): Subscription;
 }
 
-interface I$sub{
-	(event: string): Observable<any>;
-	(event: string, callback: (value: any) => void): Subscription;
-	(event: string, callback: (value: any) => void, error: (error: any) => void): Subscription;
-	(event: string, callback: (value: any) => void, error: (error: any) => void, complete: () => void): Subscription;
+interface I$sub {
+  (event: string): Observable<any>;
+  (event: string, callback: (value: any) => void): Subscription;
+  (
+    event: string,
+    callback: (value: any) => void,
+    error: (error: any) => void
+  ): Subscription;
+  (
+    event: string,
+    callback: (value: any) => void,
+    error: (error: any) => void,
+    complete: () => void
+  ): Subscription;
 }
